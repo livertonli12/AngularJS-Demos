8c27d9aa44372768c9f90660067613ced11c45e0
livertonli12 <lifelipe7@live.com>
2015-02-22 23:28:06 -0300

    Demonstrações 4, 5 e 6

---
+++ AngularDemos/Demo06/app/app.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', ['ngRoute', 'ngAnimate']);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeCtrl',
+                templateUrl: 'app/areas/home/views/index.html'
+            })
+            .otherwise({
+                controller: 'HomeCtrl as vm',
+                templateUrl: '404.html',                
+            });
+    });
+})();
---
+++ AngularDemos/Demo06/app/areas/home/controllers/HomeCtrl.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeCtrl', HomeCtrl);
+
+    HomeCtrl.$inject = ['$scope', '$location', 'CustomerRepository'];
+
+    function HomeCtrl($scope, $location, CustomerRepository) {
+        $scope.title = 'Clientes cadastrados';
+        $scope.customers = [];
+        
+        LoadCustomers();
+
+        function LoadCustomers() {
+            var promise = CustomerRepository.getAllCustomers();
+
+            promise.then(
+               function (result) {
+                   $scope.customers = result.data;
+               },
+               function (error) {
+                   console.log(error);
+               });
+        }
+    }
+})();
